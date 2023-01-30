a1f0370482652e07287ecbc23770861a4e1ef58e
ohyuchan123 <oyuchan50@gmail.com>
2023-01-30 23:56:16 +0900

    JPA를 사용하여 글 작성 처리

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,16 +1,37 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody/*반환 값을 그대로 띄워줄 수 있게 해주는 Annotation*/
-    public String main(){
-        return "Hello World";
+    @Autowired
+    private BoardService boardService;
+
+//    @GetMapping("/")
+//    @ResponseBody/*반환 값을 그대로 띄워줄 수 있게 해주는 Annotation*/
+//    public String main(){
+//        return "Hello World";
+//    }
+
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardwriteForm(){
+        return "boardwrite";
     }
 
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+//        System.out.println("제목 : "+title);
+//        System.out.println("내용 : "+content);
+
+//        System.out.println(board.getTitle());
+        boardService.write(board);
+        return "";
+    }
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,18 @@
+package com.study.board.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//jpa가 읽어들인다.
+    private Integer id;
+
+    private String title;
+
+    private String content;
+}
